ce033c58b5ab0b5c96ae6c3cd30708b3c8504d0a
Your Name <you@example.com>
2022-05-20 16:25:58 -0300

    Criação do código

---
+++ dia-3/teste.js
@@ -0,0 +1,83 @@
+console.log("Olá mundão!")
+console.log("Meu primeiro codigo!")
+
+// Isso é um comentário de uma linha
+
+/* Isso é um comentário de
+várias linhas 
+acabando aqui*/
+
+let coisas
+let coisas1, coisas2, coisas3
+
+console.log("coisas1,coisas2,coisas3")
+
+//Atribuição de valor a varável
+coisas = 'circulo'
+console.log(coisas,coisas1,coisas2,coisas3)
+coisas1 = 23
+console.log(coisas,coisas1,coisas2,coisas3)
+
+coisas2 = true
+console.log(coisas,coisas1,coisas2,coisas3)
+
+let nome
+nome = "Otacílio"
+let sobrenome = "Rezende"
+let idade = 37
+let idadeTexto = "37"
+let autura = 1.78
+let logada = false
+let logadaTexto = "false"
+
+ console.log(nome, typeof(nome))
+ console.log(sobrenome, typeof(sobrenome))
+ console.log(idade, typeof(idade))
+ console.log(idadeTexto, typeof(idadeTexto))
+ console.log(autura, typeof(autura))
+ console.log(logada, typeof(logada))
+ console.log(logadaTexto, typeof(logadaTexto))
+
+// Array
+ let lista = ['arroz', 'feijão', 'macarrão', nome, 23, false, [23,24]]
+ console.log(lista, typeof(lista))
+
+ //mapa
+ let mapa = {nome: 'Otacilio', idade: 37}
+ console.log(mapa, typeof(mapa))
+
+ let numero = 24
+ console.log(numero, typeof(numero))
+ numero = 'treze'
+ console.log(numero, typeof(numero))
+
+ let nota1 = 10
+ let nota2 = 9
+
+ let media = (nota1 + nota2) /2
+ console.log('media:', media)
+
+ let resultado = 23 + 40
+ console.log('resultado:', resultado)
+
+ let num1 = 1
+ let num2 = '2'
+ let resultadoMaluco = num1 + num2
+ console.log('resultadoMaluco', resultadoMaluco)
+ let resultadocorreto = num1 - num2
+ console.log('resultadoCorreto', resultadocorreto)
+
+ console.log(nome + " " +sobrenome)
+
+ //condicionais
+ let idadeJogador = 15
+
+ if (idadeJogador > 18){
+    //se for verdade
+    console.log('Pode jogar')
+ } else{
+    //se não for verdade
+    console.log('Não pode jogar')
+ }
+
+
